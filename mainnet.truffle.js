38bc16aa23b50105838dfa6286dd7de11ad5dd82
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-12-13 13:34:41 -0600

    Upgraded contracts, added truffle setup for deployment, ignored env, locked new dependencies for deployment

---
+++ mainnet.truffle.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var infura_apikey = process.env.INFURA_PROJECT_ID; 
+var mnemonic = process.env.MNEMONIC;
+
+var CONFIG = {
+  networks: {
+    development:  {
+      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + infura_apikey),
+      network_id: 3,
+      gas: 4500000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+};
+
+module.exports = CONFIG;
